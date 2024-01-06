fb33b83e8b9221be1cd5ec64fd37fc5d439a7490
ashtishad <ashefgo@gmail.com>
2024-01-06 06:38:37 +0600

    Enhanced utility methods and introduced constants for improved code structure.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,24 +1,27 @@
-package main
+package utils
 
 import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+
+	"github.com/Mubasshira-Tazrin/rate-limiting-api/internal/constants"
 )
 
-func generateAuthToken() string {
-	tokenLength := 32
-	randomBytes := make([]byte, tokenLength)
+func GenerateAuthToken() string {
+	randomBytes := make([]byte, constants.AuthTokenLen)
 	_, err := rand.Read(randomBytes)
+
 	if err != nil {
 		fmt.Println("Error generating random bytes:", err)
 		return ""
 	}
 
 	authToken := base64.URLEncoding.EncodeToString(randomBytes)
+
 	return authToken
 }
 
-func constructRedisKeys(authToken string) (usageKey, limitKey string) {
+func ConstructRedisKeys(authToken string) (usageKey, limitKey string) {
 	return fmt.Sprintf("global:%s:usage", authToken), fmt.Sprintf("global:%s:limit", authToken)
 }
