eda4b71b748272374f39ab482ccadfcc96e5eeb0
ashtishad <ashefgo@gmail.com>
2024-01-06 06:19:03 +0600

    Improve redis implementation.
    * Replace hard-coded critical commands such as "GET" and "SET" with constants to improve code robustness. Following the common idiom that suggests using constants for values repeated more than twice, this enhancement enhances code clarity and reduces the risk of errors.
    * err returned from external libraries should be wrapped, add a little info to make it easier to find. See my utilisation of fmt.Errorf()

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -4,4 +4,8 @@ const (
 	RedisAddress = "127.0.0.1:6379" // use 127.0.0.1 ipv4 addr, instead of localhost
 	AuthTokenLen = 32
 	ServerAddr   = "127.0.0.1:8080"
+
+	RedisCmdSet       = "SET"
+	RedisCmdGet       = "GET"
+	RedisCmdIncrement = "INCR"
 )
--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -16,34 +16,35 @@ func StoreAuthTokenInRedis(authToken string, l *slog.Logger) {
 		l.Error("error connecting to Redis", "err", err)
 		return
 	}
+
 	defer conn.Close()
 
-	_, err = conn.Do("SET", fmt.Sprintf("global:%s:limit", authToken), 100)
-	if err != nil {
+	if _, err = conn.Do(constants.RedisCmdSet, fmt.Sprintf("global:%s:limit", authToken), 100); err != nil {
 		l.Error("error storing limit in Redis", "err", err)
 	}
 
-	_, err = conn.Do("SET", fmt.Sprintf("global:%s:usage", authToken), 0)
-	if err != nil {
+	if _, err = conn.Do(constants.RedisCmdSet, fmt.Sprintf("global:%s:usage", authToken), 0); err != nil {
 		l.Error("error storing usage in Redis", "err", err)
 	}
 }
 
 func GetRedisValues(limitKey, usageKey string) (int, int, error) {
 	conn, err := redis.Dial("tcp", constants.RedisAddress)
+
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, fmt.Errorf("unable to connect redis server:%w", err)
 	}
+
 	defer conn.Close()
 
-	limit, err := redis.Int(conn.Do("GET", limitKey))
+	limit, err := redis.Int(conn.Do(constants.RedisCmdGet, limitKey))
 	if err != nil {
 		return 0, 0, err
 	}
 
-	usage, err := redis.Int(conn.Do("GET", usageKey))
+	usage, err := redis.Int(conn.Do(constants.RedisCmdGet, usageKey))
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, fmt.Errorf("unable to decode redis cmd reply:%w", err)
 	}
 
 	return limit, usage, nil
@@ -55,10 +56,10 @@ func IncrementUsage(usageKey string, l *slog.Logger) {
 		l.Error("error connecting to Redis", "err", err)
 		return
 	}
+
 	defer conn.Close()
 
-	_, err = conn.Do("INCR", usageKey)
-	if err != nil {
+	if _, err = conn.Do(constants.RedisCmdIncrement, usageKey); err != nil {
 		l.Error("error incrementing usage in Redis", "err", err)
 	}
 }
